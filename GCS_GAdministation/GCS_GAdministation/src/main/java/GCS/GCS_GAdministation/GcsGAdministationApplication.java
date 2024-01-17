9a8f8e3859765ad351b8cc793c80d8f17cb2881d
AdnaneElayachi <adnaneelayachi1@gmail.com>
2024-01-17 17:46:09 +0100

    sonar

--- GCS_GAdministation/GCS_GAdministation/src/main/java/GCS/GCS_GAdministation/GcsGAdministationApplication.java
+++ GCS_GAdministation/GCS_GAdministation/src/main/java/GCS/GCS_GAdministation/GcsGAdministationApplication.java
@@ -6,13 +6,14 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
 @EnableDiscoveryClient
+@SpringBootApplication
 public class GcsGAdministationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GcsGAdministationApplication.class, args);
 	}
+
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
